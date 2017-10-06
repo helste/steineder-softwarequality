c14dadff82eb6d44b7865d25bf24a488bd50b65e
Helmut Steineder <helmut.steineder@jipp.it>
2017-10-06 10:25:18 +0200

    Added inspection source code

---
+++ Inspection/StaticAnalysis-FindBugs/src/test/java/org/se/lab/SimpleTest.java
@@ -0,0 +1,13 @@
+package org.se.lab;
+
+import org.junit.Test;
+
+public class SimpleTest
+{
+	@Test
+    public void testSomething()
+    {
+        String s = "Hallo";
+        s.trim();
+    }
+}
