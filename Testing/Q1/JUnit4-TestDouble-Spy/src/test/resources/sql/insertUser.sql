7e7b355a56f94c58e89b4fc509fb3a094de36cb6
Helmut Steineder <helmut.steineder@jipp.it>
2017-11-30 16:18:14 +0100

    added Spy examples

---
+++ Testing/Q1/JUnit4-TestDouble-Spy/src/test/resources/sql/insertUser.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO users (firstname,lastname,username, password) VALUES ('Homer', 'Simpson', 'homer', '*******');
+INSERT INTO users (firstname,lastname,username, password) VALUES ('Bart', 'Simpson','bart', '*******');
+INSERT INTO users (firstname,lastname,username, password) VALUES ('Lisa', 'Simpson','lisa', '*******');
+
+
